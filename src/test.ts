6ec94180fbf76d4c866b9d3d3663cda7763bc906
eric2788 <tsukiko852@gmail.com>
2020-10-16 13:25:35 +0800

    test workflows

---
+++ src/test.ts
@@ -0,0 +1,7 @@
+import chat from "./secret/chat.json";
+import room from './secret/id.json';
+
+console.log('hello world!!')
+
+console.log('chat: '.concat(JSON.stringify(chat)))
+console.log('room: '.concat(JSON.stringify(room)))
