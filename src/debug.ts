60e899a682d7b6957036f969298be59b90db4282
eric2788 <tsukiko852@gmail.com>
2020-10-16 13:51:52 +0800

    fix test schema

--- src/debug.ts
+++ src/debug.ts
@@ -4,7 +4,7 @@ import BotUtils from "./utils/BotUtils";
 import auth from "./secret/auth.json";
 import room from './secret/id.json';
 import './managers/MySQLManager'
-import connection from "./managers/MySQLManager";
+import connection, {close} from "./managers/MySQLManager";
 import {version} from "./index";
 
 const client = new Discord.Client();
@@ -17,18 +17,14 @@ const debug = () => {
     } else {
         BotUtils.activate(client, guild);
         console.log(`HyperNiteMC Discord Bot ${version} has been successfully tested`)
+        console.log('closing mysql connection')
+        close().then(() => console.log('successfully closed'));
         process.exit(0);
     }
 };
 
 client.login(auth.token)
-    .then(() => connection().catch(console.error))
-    .catch(r => {
-        if (r instanceof Error) {
-            console.error(r.message);
-            process.exit(1);
-        }
-    });
+    .then(() => connection())
 
 
 client.on('ready', () => {
