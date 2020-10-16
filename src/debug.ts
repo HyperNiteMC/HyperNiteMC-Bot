b3a2ccbe2872654879864d794637525ed55932d6
eric2788 <tsukiko852@gmail.com>
2020-10-16 14:02:57 +0800

    fix workflows

--- src/debug.ts
+++ src/debug.ts
@@ -1,6 +1,5 @@
 import * as Discord from 'discord.js';
 import {Guild} from 'discord.js';
-import BotUtils from "./utils/BotUtils";
 import auth from "./secret/auth.json";
 import room from './secret/id.json';
 import './managers/MySQLManager'
@@ -9,24 +8,38 @@ import connection, {close} from "./managers/MySQLManager";
 const client = new Discord.Client();
 
 const debug = () => {
+    console.log("validating hypernite-mc guild id...")
     const guild: Guild = client.guilds.cache.get(room.guild);
     if (guild == undefined) {
         console.error("The bot has not joined the HNMC Discord Guild!");
         process.exit(1);
     } else {
-        BotUtils.activate(client, guild);
+        console.log("validated successful")
         console.log(`HyperNiteMC Discord Bot has been successfully tested`)
         console.log('closing mysql connection')
-        close().then(() => console.log('successfully closed')).finally(() => process.exit(0));
+        close().then(() => console.log('successfully closed')).catch(err => {
+            console.error(err);
+            process.exit(1);
+        }).finally(() => process.exit(0));
 
     }
 };
 
+console.log("testing login with token...")
 client.login(auth.token)
-    .then(() => connection())
+    .then(() => {
+        console.log("tested successful")
+        console.log("testing connecting to mysql...")
+        return connection()
+    })
+    .then(() => console.log("tested successful"))
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    })
 
 
 client.on('ready', () => {
-    console.log("This bot is ready to be tested");
+    console.log("This bot is now on ready state");
     debug();
 });
